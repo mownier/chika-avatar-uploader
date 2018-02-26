3ed333cbf7de3b2ad2fcef7b4835380c64a0d8ab
Mounir Ybanez <rinuom91@gmail.com>
2018-02-26 22:35:26 +0800

    Implemented view controller that holds the Scene.

--- ChikaAvatarUploader/ViewController.swift
+++ ChikaAvatarUploader/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import ChikaCore
+import ChikaFirebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    var scene: Scene!
+    
+    override func loadView() {
+        super.loadView()
+        
+        let factory = Factory(style: .person)
+        scene = factory.onProgress({
+            print($0?.fractionCompleted ?? 0)
+            
+        }).withOutput({
+            print($0)
+            
+        }).build()
+        
+        containerView.addSubview(scene.view)
+        addChildViewController(scene)
+        scene.didMove(toParentViewController: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scene.setAvatar(withDownloadURL: URL(string: ""))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        scene.view.frame = containerView.bounds
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func go(_ sender: UIBarButtonItem) {
+        scene.startUpload()
     }
-
-
+    
+    @IBAction func signOut(_ sender: UIBarButtonItem) {
+        let switcher = OfflinePresenceSwitcher()
+        let _ = switcher.switchToOffline { result in
+            print("[ChikaFirebase/Writer:OfflinePresenceSwitcher]", result)
+        }
+        
+        let action = SignOut()
+        let operation = SignOutOperation()
+        let _ = operation.withCompletion({ result in
+            switch result {
+            case .ok(let ok):
+                print("[ChikaFirebase/Auth:SignOut]", ok)
+                showSignIn()
+                
+            case .err(let error):
+                showAlert(withTitle: "Error", message: "\(error)", from: self)
+            }
+            
+        }).signOut(using: action)
+    }
+    
 }
 
